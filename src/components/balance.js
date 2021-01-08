88fac986e2527d3c476dce1ea6a42da9e2631135
rich0024 <yance0022@gmail.com>
2021-01-08 16:23:36 -0500

    balance set up

---
+++ src/components/balance.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class balance extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default balance
