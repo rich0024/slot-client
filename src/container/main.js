ef7f004f3d051026416ce50e254a165f1f9710f0
rich0024 <yance0022@gmail.com>
2021-01-08 16:19:03 -0500

    container clean up

--- src/container/main.js
+++ src/container/main.js
@@ -3,20 +3,9 @@ import React, { Component } from 'react';
 
 class Main extends Component {
 
-    displayReel = () => {
-        let arr1 = ['cherry', 'lemon', 'apple', 'lemon', 'banana', 'banana', 'lemon', 'lemon']
-        let arr2 = ['lemon', 'apple', 'lemon', 'lemon', 'cherry', 'apple', 'banana', 'lemon']
-        let arr3 = ['lemon', 'apple', 'lemon', 'apple', 'cherry', 'lemon', 'banana', 'lemon']
-
-        return (arr1.forEach(element => {
-            <p>{element}</p>
-        }));
-    }
     render() {
         return (
             <div>
-                {this.displayReel()}
-                <p>test </p>
             </div>
         )
     }
