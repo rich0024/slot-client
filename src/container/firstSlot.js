12def9ad8b35ae64323feeefb754bd9444e49a01
rich0024 <yance0022@gmail.com>
2021-01-15 22:51:57 -0500

    first slot

---
+++ src/container/firstSlot.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class firstSlot extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default firstSlot
