93ee12758db317ca7b05e889d2d9309c31d1ec63
rich0024 <yance0022@gmail.com>
2021-01-15 22:50:25 -0500

    multiple games setup

---
+++ src/container/games.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class games extends Component {
+    render() {
+        return (
+            <div>
+                <p>This will house a grid of all game apps</p>
+            </div>
+        )
+    }
+}
+
+export default games
